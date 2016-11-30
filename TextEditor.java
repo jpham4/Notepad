6bde2e7e2331438fe7caef6ce5d89b4bee7d7c5d
mashworth1 <mashworth1@student.gsu.edu>
2016-11-30 17:01:32 -0500

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -224,7 +224,7 @@ public class TextEditor extends JFrame implements ActionListener{
 				saveFile(filename);
 		} 
 		else if(event.getSource() == delete)
-			new TextEditor();
+			textArea.setText(textArea.getText().replace(textArea.getSelectedText(),""));
 		else if(event.getSource() == selectAll)
 			textArea.selectAll();
 		else if(event.getSource() == copy)
